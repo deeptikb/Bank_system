57db99ec8199dfae9a15fcd1bbe707569bae34c5
deeptikb <61556408+deeptikb@users.noreply.github.com>
2021-07-18 13:44:04 +0530

    Update Homesite.php

--- FrontEnd/Homesite.php
+++ FrontEnd/Homesite.php
@@ -1,1 +1,25 @@
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link rel="stylesheet" href="style2.css">
+</head>
+<body style="margin-top:-20px;overflow-y:hidden;">
+    <div style=" background: rgba(248,164,96,0.7);height: 4cm;">
+        <h1>Basic Banking Transactions</h1>
+        <div id="para">
+         <p1> A Secure way !</p1>
+       </div>
+    </div>
+    <div class="container"> 
+    <div id="btn"> 
+      <button class="button1"><a href="viewcustomers.php">Customers</a></button>&nbsp;&nbsp;
+      <button class="button2"><a href="viewtransactions.php">Transactions</a></button>
+</div>  
+    </div>
+</body>
+</html>
